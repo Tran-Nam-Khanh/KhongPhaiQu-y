aec1e75ca5fc2888d0a9c8a486c880aaef618056
levanhuy08092006 <levanhuy080906@gmail.com>
2025-10-18 08:24:59 +0700

    Huy thêm giao diện thắng thua và HUD

--- src/Arkanoid/ui/GameOverController.java
+++ src/Arkanoid/ui/GameOverController.java
@@ -6,25 +6,21 @@ import javafx.scene.control.Button;
 
 public class GameOverController {
 
-    // Liên kết với Button có fx:id="playAgainButton" trong FXML
+    //Button có fx:id="playAgainButton"
     @FXML
     private Button playAgainButton;
 
-    // Liên kết với Button có fx:id="mainMenuButton" trong FXML
+    // Button có fx:id="mainMenuButton"
     @FXML
     private Button mainMenuButton;
 
-    // Hàm này sẽ được gọi khi nhấn nút "Chơi Lại"
     @FXML
     void handlePlayAgainAction(ActionEvent event) {
         System.out.println("Nút Chơi Lại (Game Over) đã được nhấn!");
-        // TODO: Sau này GameManager sẽ gọi hàm restartGame() ở đây
     }
 
-    // Hàm này sẽ được gọi khi nhấn nút "Về Menu Chính"
     @FXML
     void handleMainMenuAction(ActionEvent event) {
         System.out.println("Nút Về Menu (Game Over) đã được nhấn!");
-        // TODO: Sau này GameManager sẽ gọi hàm returnToMenu() ở đây
     }
 }
--- src/Arkanoid/ui/WinScreenController.java
+++ src/Arkanoid/ui/WinScreenController.java
@@ -12,17 +12,12 @@ public class WinScreenController {
     @FXML
     private Button mainMenuButton;
 
-    // Hàm này sẽ được gọi khi nhấn nút "Chơi Lại"
     @FXML
     void handlePlayAgainAction(ActionEvent event) {
         System.out.println("Nút Chơi Lại (Win Screen) đã được nhấn!");
-        // TODO: Sau này GameManager sẽ gọi hàm restartGame() ở đây
     }
-
-    // Hàm này sẽ được gọi khi nhấn nút "Về Menu Chính"
     @FXML
     void handleMainMenuAction(ActionEvent event) {
         System.out.println("Nút Về Menu (Win Screen) đã được nhấn!");
-        // TODO: Sau này GameManager sẽ gọi hàm returnToMenu() ở đây
     }
 }
